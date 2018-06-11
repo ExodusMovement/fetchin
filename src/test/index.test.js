01fe08fc8941fa40c30c6963a0b9bd423067abb4
a <sonayen.a@gmail.com>
2018-06-12 01:15:17 +0300

    Handle more real world cases

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,4 +1,4 @@
-import { get, post } from '../lib';
+import { get, post, del, put, head } from '../lib';
 
 describe('mock core', () => {
   it('makes a simple get request', async () => {
@@ -75,13 +75,28 @@ describe('mock core', () => {
 
     expect(res.ref).toBe(true);
   });
+
+  it('makes a simple del request', async () => {
+    const res = await del('https://foo');
+    expect(res.config.method).toBe('DELETE');
+  });
+
+  it('makes a simple put request', async () => {
+    const res = await put('https://foo');
+    expect(res.config.method).toBe('PUT');
+  });
+
+  it('makes a simple head request', async () => {
+    const res = await head('https://foo');
+    expect(res.config.method).toBe('HEAD');
+  });
 });
 
 describe('core', () => {
   beforeAll(() => (process.env.NODE_ENV = 'production'));
   afterAll(() => (process.env.NODE_ENV = 'test'));
 
-  it('works', async () => {
+  it('works with json', async () => {
     const res = await get('https://api.bitfinex.com/v2/tickers', {
       params: { symbols: 'tBTCUSD' }
     });
@@ -89,5 +104,40 @@ describe('core', () => {
     const btc = res[0][7];
 
     expect(btc).toBeGreaterThan(0);
+  });
+
+  it('works with non-json', async () => {
+    const btc = await get('https://blockchain.info/q/latesthash', {
+      json: false
+    });
+
+    expect(btc.length).toBe(64);
+  });
+
+  it('returns a ref', async () => {
+    const res = await get('https://blockchain.info/q/latesthash', {
+      json: false,
+      ref: true
+    });
+
+    expect(res.ref).toBeTruthy();
+    expect(res.ref.status).toBe(200);
+    expect(res.data.length).toBe(64);
+  });
+
+  it('handles rejection', async () => {
+    global.fetch = jest
+      .fn()
+      .mockImplementation(
+        () => new Promise((res, rej) => rej(new Error('oh no')))
+      );
+
+    expect.assertions(1);
+
+    try {
+      await get('https://google.com');
+    } catch (err) {
+      expect(err.message).toEqual('oh no');
+    }
   });
 });
