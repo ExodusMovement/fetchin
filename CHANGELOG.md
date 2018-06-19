ddd54c1c43025f2e9e09848499ba5d1dce498867
a <sonayen.a@gmail.com>
2018-06-19 13:38:35 +0300

    Support react-native

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.19
+
+- Support `react-native`.
+
 # 0.0.17
 
 - Update build and test process.
---
+++ src/lib/rn.js
@@ -0,0 +1,6 @@
+import ts from './ts';
+import qs from './qs';
+
+import { get, post, del, put, head } from './core';
+
+export { ts, qs, get, post, del, put, head };
